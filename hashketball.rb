370b9b0a3197fcf5a156eea58a02ac216fb72f69
Asaf Davidov <adavidov@conncoll.edu>
2018-09-11 18:55:47 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   game_hash = {
     :home => {
       :team_name => "Brooklyn Nets", 
-      :colors => ["Black", "White"]
+      :colors => ["Black", "White"],
       :players => {
         :number => [0, 30, 11, 1, 31],
         :shoe => [16, 14, 17, 19, 15],
