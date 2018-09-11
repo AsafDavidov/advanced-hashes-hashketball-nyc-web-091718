4edaae89106c1e2fd01946453edc9be78a68d3ba
Asaf Davidov <adavidov@conncoll.edu>
2018-09-11 18:47:46 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,7 @@ def game_hash
         :shoe => [16,14,17,19,15]
         :points =>[22,12,17,26,19]
         :rebounds =>[12,12,19,12,2]
-        :assists =>
+        :assists =>[12,12,10,6,2]
         :steals =>
         :blocks =>
         :slam_dunks =>
