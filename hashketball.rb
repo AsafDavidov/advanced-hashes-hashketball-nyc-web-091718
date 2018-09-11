71811e409f89fd82d187ab8f0f82f38982d3d8c2
Asaf Davidov <adavidov@conncoll.edu>
2018-09-11 20:02:18 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -151,10 +151,8 @@ def team_colors(g_team_name)
   teamColors = []
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
-      if attribute == :team_name
-        if data == g_team_name
+      if attribute == :team_name && data == g_team_name
          teamColors = attribute[:colors]
-        end
       end
     end
   end
