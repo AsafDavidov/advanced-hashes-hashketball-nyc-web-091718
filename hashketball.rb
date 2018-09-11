8540e54f990364e92b047415a77e242a212d9b8b
Asaf Davidov <adavidov@conncoll.edu>
2018-09-11 18:56:02 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,7 @@ def game_hash
         :blocks =>[1,	12,	1,	8,	11],
         :slam_dunks =>[1,	7,	15,	5,	1]
       }
-    }
+    },
     :away => {
       :team_name => "Charlotte Hornets", 
       :colors => ["Turquoise", "Purple"],
