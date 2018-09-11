04dab229bc5d0c52f0b276a39a5f6573480ec353
Asaf Davidov <adavidov@conncoll.edu>
2018-09-11 19:09:44 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -70,7 +70,7 @@ def game_hash
           :steals =>2,
           :blocks =>7,
           :slam_dunks =>2
-        }
+        },
         "Bismak Biyombo"=> {
           :number =>0,
           :shoe =>16,
